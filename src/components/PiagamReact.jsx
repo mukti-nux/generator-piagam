6a6a04f2a205ede9bb8f09d6ae111b214230ac77
root <root@DESKTOP-HsnMkt.localdomain>
2025-06-04 13:40:49 +0700

    yo lah

--- src/components/PiagamReact.jsx
+++ src/components/PiagamReact.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import html2canvas from 'html2canvas';
 import './piagam.css';
 
 export default function PiagamReact({ nama, status }) {
+  const downloadAsImage = () => {
+    const piagamEl = document.getElementById('piagam-container');
+
+    html2canvas(piagamEl).then((canvas) => {
+      const link = document.createElement('a');
+      link.download = 'piagam.png';
+      link.href = canvas.toDataURL('image/png');
+      link.click();
+    });
+  };
+
   return (
-    <div className="piagam-container">
-      <div className="text-nama">{nama}</div>
-      <div className="text-status">{status}</div>
+    <div style={{ textAlign: 'center' }}>
+      <div id="piagam-container" className="piagam-container">
+        <div className="text-nama">{nama}</div>
+        <div className="text-status">{status}</div>
+      </div>
+      <button onClick={downloadAsImage} style={{ marginTop: '16px' }}>
+        Download PNG
+      </button>
     </div>
   );
 }
